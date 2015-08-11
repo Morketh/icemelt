1ba80f06d1fb0c7d8ea434a39b87ace5b3d7b636
Admiral Morketh <admmorketh@gmail.com>
2015-08-11 17:29:09 -0500

    adds progress Bar

--- IceMelt.py
+++ IceMelt.py
@@ -28,15 +28,41 @@ import os
 import subprocess
 import sys
 import getpass
-import time
+from time import sleep
 import glob
+import progressbar
+
+##################################################################################################
+#
+# New functions
+# TODO: move these to a set of submodules
+#
+##################################################################################################
+
+def var_dump(var, prefix=''):
+    """
+    You know you're a php developer when the first thing you ask for
+    when learning a new language is 'Where's var_dump?????'
+    """
+    my_type = '[' + var.__class__.__name__ + '(' + str(len(var)) + ')]:'
+    print(prefix, my_type)
+    prefix += '    '
+    for i in var:
+        if type(i) in (list, tuple, dict, set):
+            var_dump(i, prefix)
+        else:
+            if isinstance(var, dict):
+                print(prefix, i, ': (', var[i].__class__.__name__, ') ', var[i])
+            else:
+                print(prefix, '(', i.__class__.__name__, ') ', i)
+
 
 # lets clear our screen and give the user some information 
 subprocess.call('clear') 
 print "Welcome: " + getpass.getuser() 
 print "Icemelt copyright (C) 2015 Andrew Malone" 
 print "this install script will resume in 5 seconds"
-time.sleep(5)
+sleep(5)
 
 _IN_MYSQL_USR_ = raw_input('Database User Name [icemelt]: ')
 if _IN_MYSQL_USR_ == '':
@@ -66,13 +92,14 @@ _IN_MYSQL_DB_ = raw_input('MySQL database to use [icemelt]: ')
 if _IN_MYSQL_DB_ == '':
         _IN_MYSQL_DB_ = 'icemelt' #defualt mysql database
 
+#returns a LIST of values
 realms = glob.glob(_LOC_SQL_UPDATES_ + '*.json')
 realms = sorted(realms)
 
 print "Inililizing.......Icemelt"
-time.sleep(2)
+sleep(2)
 print "Preparing Winter Gear......"
-time.sleep(4)
+sleep(4)
 print "Activating Thermo Sensors....... [ OK ]"
 
 # data orginization function pulls a list of file names and orginizes them we should be able to extract a realm name from the file names for icemelt
@@ -88,12 +115,21 @@ print "Activating Thermo Sensors....... [ OK ]"
 # $8 = MySQL PORT
 # fname will be in the format us_cenarius_tier12_10.json
 
-time.sleep(5)
+sleep(5)
 
+#debug dump of Glob
+#var_dump(realms, '')
+
+index = 0
+bar = progressbar.ProgressBar(maxval=len(realms), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]).start()
 for fname in realms:
   # clears out chat spam and makes the output show only at the top of the screen
-  os.system("clear")
-  print "Applying Ice Scraper to: " + fname
+  #os.system("clear")
+  #print "Applying Ice Scraper to: " + fname
+  bar.update(index)
+  index = index + 1
+  
   rname = fname.split("_")[1].replace('.json', '')
   # we now need to call the ginsert script with the user provided variables
   os.system("./ginsert "+fname+" "+rname+" "+_IN_SQL_FILE_+" "+_IN_MYSQL_USR_+" "+_IN_MYSQL_PASS_+" "+_IN_MYSQL_DB_+" "+_IN_MYSQL_HOST_+" "+_IN_MYSQL_PORT_)
+bar.finish()
