34dd51a3e01797a13710f527b1376c739973bda0
Admiral Morketh <admmorketh@gmail.com>
2015-08-13 18:38:55 -0500

    adds a Sit-Rep at end of run time. also a few comments

--- Cold-air-funnel.py
+++ Cold-air-funnel.py
@@ -66,6 +66,8 @@ print "Preparing Multipoint Thermocouple Assemblies"
 # SELECT `index`,`realm`,`guild` FROM `guilds`;
 # SELECT COUNT(`index`) AS 'total' FROM `guilds`;
 
+# we want NULL status. As we get Page-Not-Found (404) errors we will be updating the database so we can filter out removed guilds
+# this also helps us update guild rosters more effciantly in the future
 _REALM_ = "SELECT `index`,`region_id`,`realm`,`guild` FROM `guilds` WHERE `status` IS NULL;"
 _INDEX_TOTAL_ = "SELECT COUNT(`index`) AS 'total' FROM `guilds` WHERE `status` IS NULL;"
 
@@ -95,8 +97,8 @@ for (index,region_id,realm,guild) in cursor:
      icemelt.commit()
      #print(url,e.code)
      #print(e.code)
- except URLError, e:
-     print 'We failed to reach a server.'
+ except urllib2.URLError, e:
+     print 'We failed to reach the server.'
      print 'Reason: ', e.reason
  else:    
      soup = BeautifulSoup(response.read())
@@ -111,6 +113,66 @@ for (index,region_id,realm,guild) in cursor:
       cursor.execute(_SQL_,sql_data)
       icemelt.commit()
  #print"./TorrentialSnowfall.sh "+realm+" "+guild.replace(" ", "_")+" "+str(index)+" "+_IN_MYSQL_USR_+" "+_IN_MYSQL_PASS_+" "+_IN_MYSQL_HOST_+" "+_IN_MYSQL_PORT_+" "+_IN_MYSQL_DB_
+
+_GUILD_STATS_ = "SELECT COUNT(DISTINCT `index`) AS 'total',`status` FROM `guilds` GROUP BY `status`;"
+_REALM_POPULATION_ = "SELECT COUNT(DISTINCT `cid`) AS 'Total Toons',`realm` FROM `chars` GROUP BY `realm` ORDER BY `Total Toons` DESC;"
+
+# Provide Guild Found Statistics on finishing up additions to the DB
+
+print " " #new line to get the stats out of the progress bar
+print "Guild statistics:"
+cursor.execute(_GUILD_STATS_)
+icemelt.commit()
+
+results = cursor.fetchall()
+
+widths = []
+columns = []
+tavnit = '|'
+separator = '+' 
+
+for cd in cursor.description:
+    widths.append(max(cd[2], len(cd[0])))
+    columns.append(cd[0])
+
+for w in widths:
+    tavnit += " %-"+"%ss |" % (w,)
+    separator += '-'*w + '--+'
+
+print(separator)
+print(tavnit % tuple(columns))
+print(separator)
+for row in results:
+    print(tavnit % row)
+print(separator)
+
+# Provide a Realm population on finishing up additions to the DB
+
+print "Population by Realm:"
+cursor.execute(_REALM_POPULATION_)
+icemelt.commit()
+
+results = cursor.fetchall()
+
+widths = []
+columns = []
+tavnit = '|'
+separator = '+'
+
+for cd in cursor.description:
+    widths.append(max(cd[2], len(cd[0])))
+    columns.append(cd[0])
+
+for w in widths:
+    tavnit += " %-"+"%ss |" % (w,)
+    separator += '-'*w + '--+'
+
+print(separator)
+print(tavnit % tuple(columns))
+print(separator)
+for row in results:
+    print(tavnit % row)
+print(separator)
 
 cursor.close()
 icemelt.close()
