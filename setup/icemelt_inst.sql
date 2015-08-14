71d02c051750c0a884a05bf5c60fe0afa19c3131
Morketh <admmorketh@gmail.com>
2015-08-13 19:44:19 -0500

    adds in char and db installs

--- setup/icemelt_inst.sql
+++ setup/icemelt_inst.sql
@@ -1,46 +1,61 @@
--- phpMyAdmin SQL Dump
--- version 3.4.11.1deb2+deb7u1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 18, 2015 at 02:17 PM
--- Server version: 5.5.43
--- PHP Version: 5.4.41-0+deb7u1
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
---
--- Database: `icemelt`
---
-
+-- --------------------------------------------------------
+-- Host:                         192.168.1.13
+-- Server version:               10.0.21-MariaDB - MariaDB Server
+-- Server OS:                    Linux
+-- HeidiSQL Version:             9.3.0.4984
 -- --------------------------------------------------------
 
---
--- Table structure for table `chars`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping database structure for icemelt
+DROP DATABASE IF EXISTS `icemelt`;
+CREATE DATABASE IF NOT EXISTS `icemelt` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `icemelt`;
+
+
+-- Dumping structure for table icemelt.chars
 DROP TABLE IF EXISTS `chars`;
 CREATE TABLE IF NOT EXISTS `chars` (
   `cid` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `gid` int(10) unsigned NOT NULL,
+  `rid` char(2) NOT NULL,
   `toon_name` varchar(15) NOT NULL,
+  `lvl` int(11) NOT NULL,
   `realm` varchar(256) NOT NULL,
   `updaded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`cid`),
-  UNIQUE KEY `toon_name` (`toon_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+-- Data exporting was unselected.
 
---
--- Table structure for table `guilds`
---
 
+-- Dumping structure for table icemelt.guilds
 DROP TABLE IF EXISTS `guilds`;
 CREATE TABLE IF NOT EXISTS `guilds` (
   `index` int(11) NOT NULL AUTO_INCREMENT,
+  `region_id` char(2) NOT NULL,
   `realm` varchar(256) NOT NULL,
   `guild` varchar(256) NOT NULL,
+  `status` int(11) DEFAULT NULL,
   PRIMARY KEY (`index`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table icemelt.regions
+DROP TABLE IF EXISTS `regions`;
+CREATE TABLE IF NOT EXISTS `regions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
