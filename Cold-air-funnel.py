fbd1ebf5310b42ea1eff99d1bce137453a196499
Morketh <admmorketh@gmail.com>
2015-08-15 22:01:45 -0500

    updates for Faction ID

--- Cold-air-funnel.py
+++ Cold-air-funnel.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
 #
-# Title: Cold-Air-Funnel
-# Purpose: Pulls guilds and realms out of icemelt DB and preps the TorrentialSnowFall script
+# Title: Cold-Air-Funnel, Stage 2 of
+#
+# Background: Ive been building and designing this script system based off of an idea from Katamitsu. 
+# According to his research notes he can manualy crossrefrance any toon on a Bnet account based on Achevments and Pet battle data.
+# taking this into account i have devised a series of scripts to "Crawl" through blizzards web page and mine out the data used for his
+# toon refrance system. 
+#
+# Purpose: Pulls guilds and realms out of icemelt DB and preps the TorrentialSnowFall subroutines.
 #
 # Copyright (C) Andrew Malone 2015
 
@@ -49,6 +55,7 @@ if _IN_MYSQL_DB_ == '':
 
 db = MySQLdb.connect(_IN_MYSQL_HOST_,_IN_MYSQL_USR_,_IN_MYSQL_PASS_,_IN_MYSQL_DB_)
 cursor = db.cursor()
+
 cursor.execute("SELECT VERSION()")
 data = cursor.fetchone()
 print "Database version: %s " % data
@@ -68,24 +75,39 @@ print "Preparing Multipoint Thermocouple Assemblies"
 
 # we want NULL status. As we get Page-Not-Found (404) errors we will be updating the database so we can filter out removed guilds
 # this also helps us update guild rosters more effciantly in the future
+# a status of 200 means FOUND and we can just UPDATE data
+# NULL will be used for not processed
+# anything else will be asumed as not found
 _REALM_ = "SELECT `index`,`region_id`,`realm`,`guild` FROM `guilds` WHERE `status` IS NULL;"
 _INDEX_TOTAL_ = "SELECT COUNT(`index`) AS 'total' FROM `guilds` WHERE `status` IS NULL;"
+
+_BAR_TOTAL_ =  "SELECT COUNT(`index`) AS 'total' FROM `guilds`;"
 
 icemelt = MySQLdb.connect(_IN_MYSQL_HOST_,_IN_MYSQL_USR_,_IN_MYSQL_PASS_,_IN_MYSQL_DB_)
 cursor = icemelt.cursor()
 
 # grab total entries
-cursor.execute(_INDEX_TOTAL_)
+cursor.execute(_BAR_TOTAL_)
+
 cursor.fetchone()
 for (total,) in cursor:
- print "Total Guilds found: "+str(total)
- bar = progressbar.ProgressBar(maxval=int(total), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage(), ' ', progressbar.ETA()]).start()
-
+ cursor.execute(_INDEX_TOTAL_)
+ (index,) = cursor.fetchone()
+ if (index,) > 0:
+  print "Total Guilds not Processed: %s out of %s" % (str(index),str(total))
+  bar = progressbar.ProgressBar(maxval=int(total), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage(), ' ', progressbar.ETA()]).start()
+ else:
+  print "All guilds seem to be have been entered into the data base"
+  print "if you would like to UPDATE guild info please use the update tool"
 # grab realm data
 cursor.execute(_REALM_)
 
 for (index,region_id,realm,guild) in cursor:
- url="http://us.battle.net/wow/"+region_id+"/guild/"+realm+"/"+guild.replace(" ", "_")+"/roster"
+ region_query = "SELECT `name` FROM `regions` WHERE `id` = %s;"
+ cursor.execute(region_query,region_id)
+ rname = cursor.fetchone()
+ print ''.join(rname)
+ url="http://us.battle.net/wow/"+''.join(rname)+"/guild/"+realm+"/"+guild.replace(" ", "_")+"/roster"
  req = urllib2.Request(url)
  bar.update(index)
 
@@ -100,15 +122,26 @@ for (index,region_id,realm,guild) in cursor:
  except urllib2.URLError, e:
      print 'We failed to reach the server.'
      print 'Reason: ', e.reason
- else:    
+ else:
+     sql = "UPDATE `icemelt`.`guilds` SET `status`=%s, `fid`=%s WHERE  `index`=%s;"
      soup = BeautifulSoup(response.read())
      x = (len(soup.findAll('tr')) - 1)
+     faction = soup.findAll(attrs={'class':"faction"})
+     for r in faction:
+       if r.string == "Horde":
+	_F_ID_ = 2
+       elif r.string == "Alliance":
+        _F_ID_ = 1
+        
+     cursor.execute(sql,[response.getcode(),index,_F_ID_])
+     icemelt.commit()
+
      for row in soup.findAll('tr')[1:x]:
       col = row.findAll('td')
       name = col[0].getText()
       level = col[3].getText()
-      _SQL_ = "INSERT INTO `chars` (`gid`, `rid`, `toon_name`, `lvl`, `realm`) VALUES (%s, %s, %s, %s, %s)" 
-      sql_data = [index,region_id,name,level,realm]
+      _SQL_ = "INSERT INTO `chars` (`gid`, `rid`, `toon_name`, `lvl`, `realm`, `fid`) VALUES (%s, %s, %s, %s, %s, %s)" 
+      sql_data = [index,region_id,name,level,realm,_F_ID_]
       #print _SQL_ % sql_data
       cursor.execute(_SQL_,sql_data)
       icemelt.commit()
@@ -173,6 +206,16 @@ print(separator)
 for row in results:
     print(tavnit % row)
 print(separator)
+
+# TODO move above code into a function so we can preform multiple status updates
+#
+# Population by Level: SELECT `lvl`, COUNT(distinct `cid`) AS 'toons', `realm` FROM `chars` GROUP BY `lvl` ORDER BY `toons` DESC;
+# Population by Region: SELECT COUNT(`cid`) AS `total`, `rid` FROM `chars` GROUP BY `rid`;
+# Population by Race:
+# Population by Class:
+# Population by Spec:
+# Population by Faction: SELECT `fid` AS 'Faction', COUNT(`cid`) AS `total` FROM `chars` GROUP BY `Faction`;
+
 
 cursor.close()
 icemelt.close()
